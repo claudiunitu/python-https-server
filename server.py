77d72359db4e90386937cb999e10197787e03fa0
claudiunitu <claudiu.nitu@acrelec.com>
2022-12-27 15:36:36 +0200

    tls encryption

--- server.py
+++ server.py
@@ -10,15 +10,16 @@ SERVER_HOST_NAME = socket.gethostname()
 SERVER_HOST_IP = socket.gethostbyname(SERVER_HOST_NAME)
 SERVER_PORT = 4433
 
-USE_TLS = True
+USE_TLS = False
 
 def startServer():
   s = HTTPServer((SERVER_HOST_IP, SERVER_PORT), HandleRequests)
 
   if USE_TLS:
-    s.socket = ssl.wrap_socket (s.socket, 
-    keyfile="./server-security/key.pem", 
-    certfile='./server-security/cert.pem', server_side=True, ssl_version=ssl.PROTOCOL_TLS)
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(certfile="./server-security/cert.pem", keyfile="./server-security/key.pem")
+    s.socket = context.wrap_socket(s.socket, server_side=True)
+  
 
   print(SERVER_HOST_IP)
   s.serve_forever()
--- serverRequestsHandler.py
+++ serverRequestsHandler.py
@@ -1,15 +1,86 @@
 from http.server import BaseHTTPRequestHandler
 
+import os
+
 
 class HandleRequests(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+    
+                    
+    def _set_headers(self, responseCode, message, contentType):
+        self.send_response(responseCode, message)
+        self.send_header('Content-type', contentType)
+        self.send_header("Access-Control-Allow-Origin","*")
+        self.send_header("Access-Control-Allow-Methods","*")
+        self.send_header("Access-Control-Allow-Headers","*")
         self.end_headers()
 
+    def getServedStorageContentType(self, path: str):
+        extension = path.split(".")
+        if(len(extension) <= 1):
+            return 'text/plain'
+        else:
+            match extension[len(extension)-1].lower():
+                case "html":
+                    return "text/html"
+                case "json":
+                    return "application/json"
+                case "js":
+                    return "text/javascript"
+                case "css":
+                    return "text/css"
+                case _:
+                    return "text/plain"
+
+
+    def serveStorageContent(self, baseHTTPRequestHandler, path: str ):
+        path = path.split('?')[0]
+        if path == "/":
+            path = "/index.html"
+        try:
+            file_to_serve = open('./storage'+path, 'rb')
+            
+            self._set_headers(200, "OK", self.getServedStorageContentType(path))
+            baseHTTPRequestHandler.wfile.write(file_to_serve.read())
+            file_to_serve.close()
+        except Exception as e:
+            self._set_headers(501, "Generic Error", "text/plain")
+        
+
+    def do_OPTIONS(self):
+        self._set_headers(200, "OK", "text/plain")
+
+    def do_HEAD(self):
+        self._set_headers(200, "OK", "text/plain")
+
+
     def do_GET(self):
-        print("received get request")
-        self._set_headers()
+        
         path = self.path
-        self.wfile.write(b"received get request: " + path.encode('utf-8'))
+        print(path)
+        self.serveStorageContent(self, path)
+        
+    
+    def do_POST(self):
+        print("received put request")
+        
+        path = self.path
+
+        filename = 'test.txt'
+        if os.path.exists(filename):
+            self._set_headers(409, 'Conflict', "text/plain")
+            reply_body = '"%s" already exists\n' % filename
+            self.wfile.write(reply_body.encode('utf-8'))
+            
+            return
+
+        file_length = int(self.headers['Content-Length'])
+        read = 0
+        with open(filename, 'wb+') as output_file:
+            while read < file_length:
+                new_read = self.rfile.read(min(66556, file_length - read))
+                read += len(new_read)
+                output_file.write(new_read)
+        self._set_headers(201, 'Created', "text/plain")
+        reply_body = 'Saved "%s"\n' % filename
+        self.wfile.write(reply_body.encode('utf-8'))
         
